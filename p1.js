f7d0e4d8dd10595241141c3ab8d14a0ea6bcdd7a
thenormalcoder <166571598+thenormalcoder@users.noreply.github.com>
2024-04-22 00:21:16 +0530

    Adding CSS files

--- p1.js
+++ p1.js
@@ -1,40 +1,41 @@
-
-        const indexbox=document.getElementById("indexbox");
-        const listcontainers=document.getElementById("third");
-        function addtask(){
-            if(indexbox.value ==='' ){
-              console.log(alert("Add your tasks for today"));
-                alert("Add your tasks for today");
+const apikey="70a30bf9a0bfae5806b8115e8ba2937b";
+        const apilink=" https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+        const searchbox=document.querySelector(".card .searchbox input");
+        const searchbtn=document.querySelector(".card .searchbox button");
+        const weathericon=document.querySelector(".rain");
+        async function check(city){
+            const response =await fetch(apilink+city+`&appid=${apikey}`);
+            if(response.status==404){
+                document.querySelector(".error").style.display="block";
+                document.querySelector(".weather").style.display="none";
             }
             else{
-                let li=document.createElement("li");
-                li.innerHTML=indexbox.value;
-                listcontainers.appendChild(li);
-                let span=document.createElement("span");
-                span.innerHTML="\u00d7";
-                li.appendChild(span);
-
-            }
-            indexbox.value="";
-            localstorage();
-        }
-
-        listcontainers.addEventListener("click",function(e) {
-            if(e.target.tagName==='LI'){
-                e.target.classList.toggle("checked");
-                localstorage();
-            }
-            else if(e.target.tagName=== "SPAN"){
-               e.target.parentElement.remove();
-               localstorage();
+                var data=await response.json();
+            console.log(data);
+           document.querySelector(".city").innerHTML=data.name; 
+           document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°C";
+           document.querySelector(".hum").innerHTML=data.main.humidity +"%"; 
+           document.querySelector(".win").innerHTML=data.wind.speed +"km/h";
+           if(data.weather[0].main=="Clouds"){
+            weathericon.src="clouds.png"
+           }
+           else if(data.weather[0].main=="Clear"){
+            weathericon.src="clear.png"
+           }
+           else if(data.weather[0].main=="Rain"){
+            weathericon.src="rain.png"
+           }
+           else if(data.weather[0].main=="Mist"){
+            weathericon.src="mist.png"
+           }
+           else if(data.weather[0].main=="Drizzle"){
+            weathericon.src="drizzle.png"
+           }
+           document.querySelector(".weather").style.display="block";
+           document.querySelector(".error").style.display="none";
             }
             
-        },false) 
-
-        function localstorage() {
-            localStorage.setItem("data",listcontainers.innerHTML)
         }
-        function showeverytime(){
-            listcontainers.innerHTML=localStorage.getItem("data");
-        }
-        showeverytime();
+        searchbtn.addEventListener("click",()=>{
+            check(searchbox.value);
+        })
